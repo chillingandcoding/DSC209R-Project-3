639ddceced59d62b6a9d34057baf16213059885f
Franky <163607462+chillingandcoding@users.noreply.github.com>
2025-10-31 21:16:49 -0700

    Updated Index, Starting JS Code, and CSS

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-d3.select();
-d3.selectAll();
-d3.select(h1).style("color", "blue");
-d3.selectAll("p").style("font-size", "18px");
+// d3.select();
+// d3.selectAll();
+// d3.select(h1).style("color", "blue");
+// d3.selectAll("p").style("font-size", "18px");
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
+async function loadData() {
+    const data = await d3.csv("./datasets/gdp.csv", d => ({
+        country: d["Country Name"],
+        gdp: +d["average_value_GDP per capita (current US$)"],
+        year: +d.Year
+    }));
+    return data;
+}
+
+function renderGraph(data) {
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+    };
+}
